3af5a1879072ef93a1b3a14338476f153af61f72
toninhowow <toninhowow@gmail.com>
2026-05-06 10:55:43 -0300

    Commit Aula12P7

--- Aula12P7/TesteAluno.java
+++ Aula12P7/TesteAluno.java
@@ -3,11 +3,11 @@ package Aula12P7;
 public class TesteAluno {
     public static void main(String[] args) {
         
-        Aluno aluno1 = new Aluno("Antônio Miranda", "2026001", 8.0, 7.5, 8.5);
+        Aluno aluno1 = new Aluno("Antônio Miranda", "A2026001", 8.0, 7.5, 8.5);
 
-        Aluno aluno2 = new Aluno("Asafe Calebe", "2026002", 5.0, 6.0, 5.5);
+        Aluno aluno2 = new Aluno("Asafe Calebe", "A2026002", 5.0, 6.0, 5.5);
 
-        Aluno aluno3 = new Aluno("Ana Carolina", "2026003", 7.0, 7.0, 7.0);
+        Aluno aluno3 = new Aluno("Ana Carolina", "A2026003", 7.0, 7.0, 7.0);
 
 
         aluno1.verificarSituacao();
